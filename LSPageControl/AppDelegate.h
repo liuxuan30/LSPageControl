5c1bd5e6a9286be986e68f38c39858efd9e5ac7c
liusen001 <nil@nildeMacBook-Air.local>
2015-09-13 10:41:43 +0800

    pagecontrol
    111

---
+++ LSPageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSPageControl
+//
+//  Created by  tsou117 on 15/8/28.
+//  Copyright (c) 2015年  tsou117. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
